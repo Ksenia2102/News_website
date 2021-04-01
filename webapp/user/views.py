7054fe5b90e632dc67dc8394bc65adf18861f225
Ksenia <eternidad@inbox.ru>
2021-04-01 18:00:04 +0300

    Added Blueprint structure

---
+++ webapp/user/views.py
@@ -0,0 +1,34 @@
+from webapp.user.forms import LoginForm
+from webapp.user.models import User
+from flask_login import current_user, login_user, logout_user
+from flask import flash, redirect, render_template, url_for, Blueprint
+
+blueprint = Blueprint('user', __name__, url_prefix='/users')
+
+@blueprint.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = 'Авторизация'
+    login_form = LoginForm()
+    return render_template('user/login.html', page_title=title, form=login_form)
+
+
+@blueprint.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы вошли на сайт')
+            return redirect(url_for('news.index'))
+    flash('Неправильное имя пользователя или пароль')
+    return redirect(url_for('login'))
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('news.index'))
