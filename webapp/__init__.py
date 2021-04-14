c95c905058cd39efca776fb1b49c4038dd3299f9
Ksenia <eternidad@inbox.ru>
2021-04-14 21:13:02 +0300

    added registration form and validators

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from flask import Flask, flash, redirect, render_template, url_for
 from flask_login import LoginManager, current_user, login_required
+from flask_migrate import Migrate
 
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.db import db
@@ -16,6 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
-from webapp.user.forms import LoginForm
-from webapp.user.models import User
+from flask import Blueprint, flash, redirect, render_template, url_for
 from flask_login import current_user, login_user, logout_user
-from flask import flash, redirect, render_template, url_for, Blueprint
+from webapp.db import db
+from webapp.user.forms import LoginForm, RegistrationForm
+from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -25,10 +26,36 @@ def process_login():
             flash('Вы вошли на сайт')
             return redirect(url_for('news.index'))
     flash('Неправильное имя пользователя или пароль')
-    return redirect(url_for('login'))
+    return redirect(url_for('user.login'))
 
 
 @blueprint.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('news.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    form = RegistrationForm()
+    title = "Регистрация"
+    return render_template('user/registration.html', page_title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f'Ошибка в поле "{getattr(form, field).label.text}": - {error}')
+        return redirect(url_for('user.register'))
